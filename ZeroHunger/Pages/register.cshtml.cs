f6542d77b8f57cddf3a88383fe363cc88c8ba8e4
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-15 01:05:38 +0800

    Add files via upload

---
+++ ZeroHunger/Pages/register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZeroHunger.Pages
+{
+    public class registerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
