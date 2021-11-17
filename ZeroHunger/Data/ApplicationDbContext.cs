115d1add61a41ba4e274fa6a425b10fb9dcb7185
Lim Sin Jie <84025799+SinJie00@users.noreply.github.com>
2021-11-18 04:44:57 +0800

    Add files via upload

---
+++ ZeroHunger/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ZeroHunger.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ZeroHunger.Data
+{
+    public class ApplicationDbContext : DbContext
+    {   
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+        public DbSet<Delivery> Delivery { get; set; }
+        public DbSet<Product>Product { get; set; }
+    }
+}
