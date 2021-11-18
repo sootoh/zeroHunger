e1f4408feea1007958aceffa061ff9e39f0c37d7
RICKY <86287753+Ricky-karunakaran@users.noreply.github.com>
2021-11-18 13:33:13 +0800

    uploadData

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ZeroHunger.Model;
+
+namespace ZeroHunger.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DryFoodDonation> DryFoodDonation { get; set; }
+    }
+}
---
+++ Data/Migrations/20211117142837_AddDryFoodDonationToDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZeroHunger.Data.Migrations
+{
+    public partial class AddDryFoodDonationToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DryFoodDonation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DryFoodName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DryFoodQuantity = table.Column<int>(type: "int", nullable: false),
+                    DryFoodPickDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DryFoodRemark = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DonorId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DelivererId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DryFoodDonation", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DryFoodDonation");
+        }
+    }
+}
