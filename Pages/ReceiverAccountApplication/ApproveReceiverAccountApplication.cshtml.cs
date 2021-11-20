da09bbb819a54e9265c792969180fa880845444f
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-20 17:52:45 +0800

    Add files via upload

---
+++ Pages/ReceiverAccountApplication/ApproveReceiverAccountApplication.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZeroHunger.Pages.ReceiverAccountApplication
+{
+    public class ApproveReceiverAccountApplicationModel : PageModel
+    {
+        public void OnGet(string? id)
+        {
+        }
+    }
+}
