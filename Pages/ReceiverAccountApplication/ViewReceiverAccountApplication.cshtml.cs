e7992924292227cbf61b9c344287cb366b6404ba
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-20 18:06:05 +0800

    Update ViewReceiverAccountApplication.cshtml.cs

--- Pages/ReceiverAccountApplication/ViewReceiverAccountApplication.cshtml.cs
+++ Pages/ReceiverAccountApplication/ViewReceiverAccountApplication.cshtml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ZeroHunger.Data;
-using ZeroHunger.Models;
+using ZeroHunger.Model;
 
 namespace ZeroHunger.Pages
 {
