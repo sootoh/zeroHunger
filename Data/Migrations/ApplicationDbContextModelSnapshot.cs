66ef3283b8a1955f7a26683132cabfd3b1f99ccf
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-23 23:33:02 +0800

    Update ApplicationDbContextModelSnapshot.cs
    add receiver

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -303,6 +303,90 @@ namespace ZeroHunger.Data.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
+                
+                modelBuilder.Entity("ZeroHunger.Model.ReceiverFamily", b =>
+            {
+                b.Property<string>("familyIC")
+                    .HasColumnType("nvarchar(450)");
+
+                b.Property<string>("familyDOB")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("familyOccupation")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("familySalaryGroupID")
+                    .HasColumnType("int");
+
+                b.Property<string>("receiverIC")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("familyIC");
+
+                b.ToTable("ReceiverFamily");
+            });
+
+            modelBuilder.Entity("ZeroHunger.Model.SalaryGroup", b =>
+            {
+                b.Property<int>("salaryGroupID")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int")
+                    .UseIdentityColumn();
+
+                b.Property<string>("salaryRange")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("salaryGroupID");
+
+                b.ToTable("SalaryGroup");
+            });
+
+            modelBuilder.Entity("ZeroHunger.Models.Receiver", b =>
+            {
+                b.Property<string>("receiverIC")
+                    .HasColumnType("nvarchar(450)");
+
+                b.Property<int>("applicationStatusID")
+                    .HasColumnType("int");
+
+                b.Property<string>("receiverAdrs1")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("receiverAdrs2")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("receiverDOB")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("receiverEmail")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("receiverFamilyNo")
+                    .HasColumnType("int");
+
+                b.Property<string>("receiverGender")
+                    .IsRequired()
+                    .HasColumnType("nvarchar(1)");
+
+                b.Property<string>("receiverName")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("receiverOccupation")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("receiverPhone")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("receiverSalaryGroupID")
+                    .HasColumnType("int");
+
+                b.Property<int>("userID")
+                    .HasColumnType("int");
+
+                b.HasKey("receiverIC");
+
+                b.ToTable("Receiver");
+            });
 #pragma warning restore 612, 618
         }
     }
