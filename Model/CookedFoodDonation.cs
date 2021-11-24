2a2f8c56f59cbc432ff0822d8a28a3561c5ccee3
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-24 23:36:04 +0800

    add cookfood model

---
+++ Model/CookedFoodDonation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Zero_Hunger.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zero_Hunger.Model
+{
+    public class CookedFoodDonation
+    {
+        [Key]
+        public int CookID { get; set; }
+        [Required]
+        public string CookName { get; set; }
+        [Required]
+        public int CookQuantity { get; set; }
+        [Required]
+        public float CookLongtitude { get; set; }
+        [Required]
+        public float CookLatitude { get; set; }
+        [Required]
+        public string DonorId { get; set; }
+    }
+}
