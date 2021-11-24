d03e01fac88c380e65cc6d34be1a84ad87efe959
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-24 23:36:34 +0800

    add user&userType model

---
+++ Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Zero_Hunger.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zero_Hunger.Model
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        [ForeignKey("FK_UserType"), Required]
+        public int UserType { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string UserPwd { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public string UserPhone { get; set; }
+        public DateTime UserBirth { get; set; }
+        public string UserAdrs1 { get; set; }
+        public string UserAdrs2 { get; set; }
+        //public virtual ICollection<Delivery>? Deliveries { set; get; }
+
+    }
+}
---
+++ Model/UserType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zero_Hunger.Model
+{
+    public class UserType
+    {
+        [Key]
+        public int typeID { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string typeName { get; set; }
+        [MaxLength(255)]
+
+        public string typeInterface { get; set; }
+       
+        
+    }
+}
