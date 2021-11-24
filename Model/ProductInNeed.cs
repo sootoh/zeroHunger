7d69ddb4751e5d1f07d7bbe31eb2bdafe3437306
TANCT <86305039+TANCT@users.noreply.github.com>
2021-11-24 16:57:00 +0800

    Add files via upload

---
+++ Model/ProductInNeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace zeroHunger.Model
+{
+    public class ProductInNeed
+    {
+        [Key]
+        public int product_id { get; set; }
+        [Required]
+        public string product_name { get; set; }
+        public string product_description { get; set; }
+        [Required]
+        public int amount { get; set; }
+        
+        [Required]
+        public string visibility { get; set; }
+        [DisplayName("Image")]
+        public string image { get; set; }
+        [NotMapped]
+        [DisplayName("Upload File")]
+        public IFormFile ImageFile { get; set; }
+
+
+
+    }
+}
