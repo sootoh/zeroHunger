ccf7daab4986a6c5b294da67eb5978b388daf24c
TANCT <86305039+TANCT@users.noreply.github.com>
2021-11-24 21:32:31 +0800

    Update ProductInNeed.cs

--- Model/ProductInNeed.cs
+++ Model/ProductInNeed.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace zeroHunger.Model
+namespace ZeroHunger.Model
 {
     public class ProductInNeed
     {
