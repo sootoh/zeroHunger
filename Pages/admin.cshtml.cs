e163f68097fba5033ba36d58bf82b7d23be1fbfb
TANCT <86305039+TANCT@users.noreply.github.com>
2021-11-24 21:31:22 +0800

    Update admin.cshtml.cs

--- Pages/admin.cshtml.cs
+++ Pages/admin.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace zeroHunger.Pages
+namespace ZeroHunger.Pages
 {
     public class adminModel : PageModel
     {
