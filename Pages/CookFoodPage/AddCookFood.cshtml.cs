95f49339edc7abcc0c608acf034251f5b62de3a2
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-24 23:37:08 +0800

    add cookfood page

---
+++ Pages/CookFoodPage/AddCookFood.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zero_Hunger.Model;
+
+namespace Zero_Hunger.Pages.CookFoodPage
+{
+    public class AddCookFoodModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public AddCookFoodModel(ApplicationDbContext db)
+        {
+            _db = db;  
+        }
+        [BindProperty]
+        public CookedFoodDonation cook { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            //userObj.UserID = "Lalisa";
+            if (ModelState.IsValid)
+            {
+                await _db.CookedFoodDonation.AddAsync(cook);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("CookFoodView");
+            }
+            else
+            {
+
+                return RedirectToPage("../Index");
+            }
+        }
+        
+    }
+}
