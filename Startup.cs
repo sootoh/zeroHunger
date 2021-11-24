599e94ecd124e3a37382934d8e4b9cf1117616dd
TANCT <86305039+TANCT@users.noreply.github.com>
2021-11-24 16:54:29 +0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -33,8 +33,9 @@ namespace ZeroHunger
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
-            services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddRazorPages().AddRazorRuntimeCompilation();;
+            
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
