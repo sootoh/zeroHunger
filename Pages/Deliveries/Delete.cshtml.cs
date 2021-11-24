7c70ef50fd08ed3755d22beee9b68d0d8a74dccb
Lim Sin Jie <84025799+SinJie00@users.noreply.github.com>
2021-11-25 00:19:44 +0800

    Add files via upload

---
+++ Pages/Deliveries/Delete.cshtml.cs
@@ -0,0 +1,35 @@
+using ZeroHunger.Data;
+using ZeroHunger.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZeroHunger.Pages.Deliveries
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Delivery Delivery { set; get; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Delivery = _db.Delivery.Find(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            var deliveryFromDb = _db.Delivery.Find(Delivery.DeliveryID);
+            if (deliveryFromDb != null)
+            {
+                _db.Delivery.Remove(deliveryFromDb);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Delivery request deleted successfully";
+                return RedirectToPage("Index");
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Pages/Deliveries/Edit.cshtml.cs
@@ -0,0 +1,49 @@
+using ZeroHunger.Data;
+using ZeroHunger.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZeroHunger.Pages.Deliveries
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Delivery Delivery { set; get; }
+        public SelectList VolunteerList { get; set; }
+        public SelectList ReceiverList { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task OnGet(int id)
+        {
+            var items = await _db.User.Where(u => u.UserType == 2).ToListAsync();
+            VolunteerList = new SelectList(items, "UserId", "UserName");
+            var bitems = await _db.User.Where(u => u.UserType == 3).ToListAsync(); ;
+            ReceiverList = new SelectList(bitems, "UserId", "UserName");
+
+            Delivery = _db.Delivery.Find(id);
+            //Category = _db.Category.FirstOrDefault(u=>u.Id==id);
+            //Category = _db.Category.SingleOrDefault(u=>u.Id==id);
+            //Category = _db.Category.Where(u => u.Id == id).FirstOrDefault();
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            /*if (Delivery.Name == Delivery.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The DisplayOrder cannot exactly match the Name.");
+            }*/
+            if (ModelState.IsValid)
+            {
+                _db.Delivery.Update(Delivery);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Delivery request updated successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
