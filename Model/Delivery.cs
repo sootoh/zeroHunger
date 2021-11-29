bb45b415d24fa9d712d754b26a96ef73236fed22
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-29 20:30:39 +0800

    Update Delivery.cs

--- Model/Delivery.cs
+++ Model/Delivery.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using ZeroHunger.Model;
+//using ZeroHunger.Model;
 
 namespace ZeroHunger.Model
 {
