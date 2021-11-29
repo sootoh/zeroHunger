59ee23e6941228e3140716a7c6037f2ea626d407
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-29 22:54:02 +0800

    Update EditUser.cshtml.cs

--- Pages/UserPage/EditUser.cshtml.cs
+++ Pages/UserPage/EditUser.cshtml.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Zero_Hunger.Model;
+using ZeroHunger.Data;
+using ZeroHunger.Model;
 
 namespace Zero_Hunger.Pages.UserPage
 {
