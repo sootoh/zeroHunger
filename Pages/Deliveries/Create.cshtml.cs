6515c506b8c18d9ee121b5c0392ebda38c15f477
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-29 20:38:56 +0800

    Update Create.cshtml.cs

--- Pages/Deliveries/Create.cshtml.cs
+++ Pages/Deliveries/Create.cshtml.cs
@@ -4,7 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace ZeroHunger.Pages.Deliveries
 {
