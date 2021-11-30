2f4eb591c92c4c84c15fc18d91a27f3f50593800
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-30 21:55:54 +0800

    Add files via upload

--- Pages/ReceiverAccountApplication/ApproveReceiverAccountApplication.cshtml.cs
+++ Pages/ReceiverAccountApplication/ApproveReceiverAccountApplication.cshtml.cs
@@ -16,6 +16,7 @@ namespace ZeroHunger.Pages
 
         }
         public Receiver application { get; set; }
+        public User user { get; set; }
         public string message { get; set; } = "";
 
         public async Task OnGetAsync(string? id)
@@ -26,10 +27,16 @@ namespace ZeroHunger.Pages
                 application = _db.Receiver.Where(i => i.receiverIC.Equals(id)).Single();
                 application.applicationStatusID = 2;
 
-                //_db.User.Add(application);
-                //int userID = _db.User.Where(i => i.email.Equals(application.receiverEmail)).Single().userID;
+                user.UserType = (_db.UserType.Where(b => b.TypeID.Equals(1)).FirstOrDefault());
+                user.UserName = application.receiverName;
+                user.UserAdrs1 = application.receiverAdrs1;
+                user.UserAdrs2=application.receiverAdrs2;
+                user.UserPhone = application.receiverPhone;
 
-                //application.userID = userID;
+                await _db.User.AddAsync(user);
+
+                application.user=_db.User.Where(i=>i.UserEmail.Equals(application.receiverEmail)).FirstOrDefault();
+                application.userID = application.user.UserID;
                 _db.Receiver.Update(application);
 
                 await _db.SaveChangesAsync();
