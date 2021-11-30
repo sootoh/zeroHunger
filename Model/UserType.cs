960798894300dacfaf0ac2e3bd76d63e0bc198b4
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-30 22:45:59 +0800

    Update UserType.cs

--- Model/UserType.cs
+++ Model/UserType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
-namespace Zero_Hunger.Model
+namespace ZeroHunger.Model
 {
     public class UserType
     {
