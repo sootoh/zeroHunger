468a6a935d153e8b0d484dd55b7debf5ad45e2f6
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-30 18:57:35 +0800

    add cookfoodPage

---
+++ Pages/CookFoodPage/AddCookFood.cshtml.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Zero_Hunger.Model;
+
+namespace Zero_Hunger.Pages.CookFoodPage
+{
+    public class AddCookFoodModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public AddCookFoodModel(ApplicationDbContext db)
+        {
+            _db = db;  
+        }
+        [BindProperty]
+        public CookedFoodDonation cook { get; set; }
+        [BindProperty]
+        public User donor { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            cook.DonorId= (_db.User.Where(b => b.UserEmail.Equals(@User.Identity.Name)).FirstOrDefault());
+            cook.DonorUserID = cook.DonorId.UserID;
+            cook.Reservation = 0;
+            cook.RemainQuantity = cook.CookQuantity;
+            
+            //if (ModelState.IsValid)
+            //{
+                await _db.CookedFoodDonation.AddAsync(cook);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("CookFoodView");
+            //}
+            //else
+            //{
+
+            //    return RedirectToPage("../Index");
+            //}
+        }
+        
+    }
+}
---
+++ Pages/CookFoodPage/CookFoodView.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using Zero_Hunger.Model;
+
+namespace Zero_Hunger.Pages.CookFoodPage
+{
+    public class CookFoodViewModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CookFoodViewModel(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IEnumerable<CookedFoodDonation> CookedFood { get; set; }
+        //public IEnumerable<User> donor { get; set; }
+        public async Task OnGet()
+        {
+            CookedFood = await _db.CookedFoodDonation.ToListAsync();
+        }
+    }
+}
