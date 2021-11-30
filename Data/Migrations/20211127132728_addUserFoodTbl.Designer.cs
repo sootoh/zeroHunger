73bc15d6f93fdb29c31a02dbd9bdca8723ac7c5e
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-30 22:48:11 +0800

    Update 20211127132728_addUserFoodTbl.Designer.cs

--- Data/Migrations/20211127132728_addUserFoodTbl.Designer.cs
+++ Data/Migrations/20211127132728_addUserFoodTbl.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Zero_Hunger.Model;
+using ZeroHunger.Model;
 
 namespace Zero_Hunger.Migrations
 {
