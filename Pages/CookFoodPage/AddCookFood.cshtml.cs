f9d24d56455045c26030dd84f9f836969b718933
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-30 18:58:22 +0800

    Update AddCookFood.cshtml.cs

--- Pages/CookFoodPage/AddCookFood.cshtml.cs
+++ Pages/CookFoodPage/AddCookFood.cshtml.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Zero_Hunger.Model;
+using ZeroHunger.Model;
 
-namespace Zero_Hunger.Pages.CookFoodPage
+namespace ZeroHunger.Pages.CookFoodPage
 {
     public class AddCookFoodModel : PageModel
     {
