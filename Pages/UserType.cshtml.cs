b6b884f3c4e2ecd137adb743db917f1e1062fece
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-30 19:04:55 +0800

    add usertypePage

---
+++ Pages/UserType.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zero_Hunger.Pages
+{
+    public class userTypeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
