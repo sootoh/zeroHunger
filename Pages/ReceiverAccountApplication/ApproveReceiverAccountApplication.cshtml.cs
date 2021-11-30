5e48adb2ada138cc7f64f9558a94e07286cc6816
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-11-30 22:28:38 +0800

    Add files via upload

--- Pages/ReceiverAccountApplication/ApproveReceiverAccountApplication.cshtml.cs
+++ Pages/ReceiverAccountApplication/ApproveReceiverAccountApplication.cshtml.cs
@@ -16,26 +16,36 @@ namespace ZeroHunger.Pages
 
         }
         public Receiver application { get; set; }
+        [BindProperty]
         public User user { get; set; }
         public string message { get; set; } = "";
 
         public async Task OnGetAsync(string? id)
         {
             application= new Receiver();
+            user= new User();
             if(id!=null)
             {
                 application = _db.Receiver.Where(i => i.receiverIC.Equals(id)).Single();
                 application.applicationStatusID = 2;
 
-                user.UserType = (_db.UserType.Where(b => b.TypeID.Equals(1)).FirstOrDefault());
+                user.UserType = (UserType)(_db.UserType.Where(b => b.TypeID.Equals(1)).FirstOrDefault());
                 user.UserName = application.receiverName;
+                user.UserPwd = "123";
+                user.UserEmail=application.receiverEmail;
+                int year = int.Parse(application.receiverDOB.Substring(0, 4));
+                int month = int.Parse(application.receiverDOB.Substring(5, 2));
+                int day = int.Parse(application.receiverDOB.Substring(8, 2));
+                user.UserBirth = new System.DateTime(year,month,day);
                 user.UserAdrs1 = application.receiverAdrs1;
                 user.UserAdrs2=application.receiverAdrs2;
                 user.UserPhone = application.receiverPhone;
 
                 await _db.User.AddAsync(user);
+                await _db.SaveChangesAsync();
 
-                application.user=_db.User.Where(i=>i.UserEmail.Equals(application.receiverEmail)).FirstOrDefault();
+                application.user=new User();
+                application.user=_db.User.Where(i=>i.UserEmail.Equals(application.receiverEmail)).Single();
                 application.userID = application.user.UserID;
                 _db.Receiver.Update(application);
 
