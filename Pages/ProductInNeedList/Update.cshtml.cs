729a72b00ad6b47256b72e8cd2d04bdfac871779
TANCT <86305039+TANCT@users.noreply.github.com>
2021-11-30 20:37:57 +0800

    Update Update.cshtml.cs

--- Pages/ProductInNeedList/Update.cshtml.cs
+++ Pages/ProductInNeedList/Update.cshtml.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using ZeroHunger.Data;
 using ZeroHunger.Model;
@@ -10,27 +14,66 @@ namespace ZeroHunger.Pages.ProductInNeedList
     public class UpdateModel : PageModel
     {
         private ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public UpdateModel(ApplicationDbContext db)
+        public UpdateModel(ApplicationDbContext db,IWebHostEnvironment hostEnvironment)
         {
             _db = db;
+            this._hostEnvironment = hostEnvironment;
 
         }
         [BindProperty]
         public ProductInNeed product { get; set; }
+        [BindProperty]
+        public IFormFile ImageFile { get; set; }
+
         public async Task OnGet(int id) => product = await _db.ProductInNeed.FindAsync(id);
+
+        private string ProcessUploadFile()
+        {
+            string uniqueFileName = null;
+            if (ImageFile != null)
+            {
+                string uploadsFolder = Path.Combine(_hostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(ImageFile.FileName);
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    ImageFile.CopyTo(fileStream);
+
+                }
+
+
+            }
+            return uniqueFileName;
+
+        }
         public async Task<IActionResult> OnPost()
         {
+            
 
             if (ModelState.IsValid)
             {
 
                 var productFromDB = await _db.ProductInNeed.FindAsync(product.product_id);
+
                 productFromDB.product_name = product.product_name;
                 if (product.product_description != null)
                     productFromDB.product_description = product.product_description;
 
                 productFromDB.amount = product.amount;
+                productFromDB.visibility = product.visibility;
+
+                if (ImageFile != null)
+                {
+                    if (productFromDB.image != null)
+                    {
+                        string filepath = Path.Combine(_hostEnvironment.WebRootPath, "images",productFromDB.image );
+                        System.IO.File.Delete(filepath);
+                    }
+                    productFromDB.image = ProcessUploadFile();
+
+                }
 
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
