a7ccbdc53cded0f451141133b669d432f1e57673
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-30 19:05:35 +0800

    Update UserType.cshtml.cs

--- Pages/UserType.cshtml.cs
+++ Pages/UserType.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Zero_Hunger.Pages
+namespace ZeroHunger.Pages
 {
     public class userTypeModel : PageModel
     {
