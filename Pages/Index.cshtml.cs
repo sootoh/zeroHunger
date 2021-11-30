4488f173e24812ffe32435094f7f853c20561d48
TANCT <86305039+TANCT@users.noreply.github.com>
2021-11-30 20:38:44 +0800

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,40 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using ZeroHunger.Helpers;
 using ZeroHunger.Model;
+using ZeroHunger.Data;
 
-namespace Zero_Hunger.Pages
+namespace ZeroHunger.Pages
 {
-    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        public User user { get; set; }
-        public int? age { get; set; }
 
-        public string username { get; set; }
+        private readonly ApplicationDbContext _db;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<ProductInNeed> products { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-            //ID = HttpContext.Session.GetString("Name");
-            HttpContext.Session.SetInt32("age", 20);
-            HttpContext.Session.SetString("username", "abc");
-            //SessionHelper.SetObjectAsJson(HttpContext.Session, "user", user);
-            age = HttpContext.Session.GetInt32("age");
-            username = HttpContext.Session.GetString("username");
-            //user = SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "user");
+            products = await _db.ProductInNeed.Where(e => e.visibility == "Visible").ToListAsync();
         }
     }
 }
