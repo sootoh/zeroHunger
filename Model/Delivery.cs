95834b1c5843280e2ea26b0971c40d9758b8e012
Lim Sin Jie <84025799+SinJie00@users.noreply.github.com>
2021-11-30 20:34:58 +0800

    Add files via upload

---
+++ Model/Delivery.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Zero_Hunger.Model;
+
+namespace ZeroHunger.Model
+{
+    public class Delivery
+    {
+        [Key]
+        [Display(Name = "Delivery ID")]
+        public int DeliveryID { get; set; }
+        [Display(Name = "Delivery Status")]
+        public DeliveryStatus DeliveryStatus { get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Delivery Time")]
+        public DateTime? DeliveryTime { get; set; }
+        public int? VolunteerID { get; set; }
+        public int? ReceiverID { get; set; }
+        //public int? DryFoodID { get; set; }
+        [ForeignKey("VolunteerID")]
+        public virtual User? Volunteer { get; set; }
+        [ForeignKey("ReceiverID")]
+        public virtual User? Receiver { get; set; }
+        //[ForeignKey("DryFoodID")]
+        public virtual ICollection<DryFoodDonation>? DryFoodDonations { get; set; }
+    }
+}
