2fbfe06f902b40155a42fbf6ed57360ba2833261
sootoh <65179455+sootoh@users.noreply.github.com>
2021-11-30 19:00:30 +0800

    Update EditUser.cshtml.cs

--- Pages/UserPage/EditUser.cshtml.cs
+++ Pages/UserPage/EditUser.cshtml.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Zero_Hunger.Model;
+using ZeroHunger.Model;
 
-namespace Zero_Hunger.Pages.UserPage
+namespace ZeroHunger.Pages.UserPage
 {
     public class EditUserModel : PageModel
     {
