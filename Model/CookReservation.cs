468937e9ef8ebf37900418c9e484a9ab7abec109
RICKY <86287753+Ricky-karunakaran@users.noreply.github.com>
2021-12-01 23:43:15 +0800

    Add files via upload

---
+++ Model/CookReservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZeroHunger.Model
+{
+    public class CookReservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int cookId { get; set; }
+        [Required]
+        public string date { get; set; }
+        [ForeignKey("resRefCook")]
+        public virtual CookedFoodDonation resRefCook { get; set; }
+
+    }
+}
