b1727f61e055cb5e1c58a5a8213b6ad9585a1071
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2021-12-10 02:33:45 +0800

    Add files via upload

---
+++ Controllers/ReceiverAccountApplicationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZeroHunger.Data;
+using ZeroHunger.Model;
+
+namespace ZeroHunger.Controllers
+{
+    [Route("api/ReceiverAccountApplication")]
+    [ApiController]
+    public class ReceiverAccountApplicationController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ReceiverAccountApplicationController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public IList<Receiver> applicationList { get; set; } = new List<Receiver>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            foreach (Receiver receiver in _db.Receiver)
+            {
+                if (receiver.applicationStatusID < 2)
+                {
+                    receiver.receiverSalaryGroup=_db.SalaryGroup.Where(i => i.salaryGroupID.Equals(receiver.receiverSalaryGroupID)).Single();
+                    applicationList.Add(receiver);
+                }
+
+            }
+            return Json(new { data = applicationList });
+        }
+    }
+}
