f0c447ef506d50b6ba876ff3bbd5ebc193cd9832
sootoh <65179455+sootoh@users.noreply.github.com>
2021-12-13 20:07:20 +0800

    Add files via upload

---
+++ wwwroot/js/cookFood.js
@@ -0,0 +1,106 @@
+﻿var foodTable;
+
+$(document).ready(function () {
+    loadFoodTable();
+});
+
+function loadFoodTable() {
+    foodTable = $('#food_load').DataTable({
+        "ajax": {
+            "url": "/api/cookfood",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            
+            /*{ "data": "cookID", "width": "5%" },*/
+            { "data": "cookName", "width": "8%" },
+            { "data": "cookQuantity", "width": "5%" },
+            
+            //{ "data": "cookLongtitude", "width": "5%" },
+            //{ "data": "cookLatitude", "width": "5%" },
+            
+            { "data": "openDate", "width": "8%" },
+            { "data": "closeDate", "width": "8%" },
+           
+            { "data": "remainQuantity", "width": "5%" },
+
+            { "data": "reservation", "width": "5%" },
+            { "data": "shopName", "width": "10%" },
+            {
+                "data": "cookID",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/CookFoodPage/EditCookFood?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:90px; margin-bottom:5px;'>
+                 Edit <i class="bi bi-pencil-square"></i>
+                        </a>
+                        <a class='btn btn-info text-white' style='cursor:pointer; width:90px;margin-bottom:5px;'
+                             onclick=End('/api/cookfood?id='+${data})>
+                            End <i class="bi bi-x-octagon"></i>
+                        </a>
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:90px;margin-bottom:5px;'
+                             onclick=Delete('/api/cookfood?id='+${data})>
+               Delete <i class="bi bi-trash-fill"></i>
+                        </a>
+                        </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function End(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once ended, the donation will not be displayed on map.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willEnd) => {
+        if (willEnd) {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        foodTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        foodTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
---
+++ wwwroot/js/userList.js
@@ -0,0 +1,76 @@
+﻿var userTable;
+
+$(document).ready(function () {
+    loadUserTable();
+});
+
+function loadUserTable() {
+    userTable = $('#user_load').DataTable({
+        "ajax": {
+            "url": "/api/userlist",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            
+            { "data": "userID", "width": "5%" },
+            { "data": "typeId", "width": "5%" },
+            { "data": "userName", "width": "5%" },
+            
+            { "data": "userPwd", "width": "5%" },
+            { "data": "userEmail", "width": "5%" },
+            
+            { "data": "userPhone", "width": "5%" },
+            
+            { "data": "userBirth", "width": "8%" },
+            { "data": "userAdrs1", "width": "10%" },
+            { "data": "userAdrs2", "width": "10%" },
+
+            {
+                "data": "userID",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/UserPage/EditUser?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Edit
+                        </a>
+                        &nbsp;
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                             onclick=Delete('/api/userList?id='+${data})>
+                            Delete
+                        </a>
+                        </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
