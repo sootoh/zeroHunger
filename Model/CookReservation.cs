2b64e0e5bab5df95adbe770b11500bc92be9592f
RICKY <86287753+Ricky-karunakaran@users.noreply.github.com>
2021-12-15 00:44:38 +0800

    Add files via upload

--- Model/CookReservation.cs
+++ Model/CookReservation.cs
@@ -13,10 +13,14 @@ namespace ZeroHunger.Model
         public int Id { get; set; }
         [Required]
         public int cookId { get; set; }
+        public int userId { get; set; }
         [Required]
         public string date { get; set; }
+        public string status { get; set; }
         [ForeignKey("resRefCook")]
-        public virtual CookedFoodDonation resRefCook { get; set; }
+        public virtual CookedFoodDonation reservationRefCook { get; set; }
+        [ForeignKey("resRefUser")]
+        public virtual User reservationRefUser { get; set; }
 
     }
 }
