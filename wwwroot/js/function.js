590bc4254b1d97b39d6b31078716b117bfdaa4e1
RICKY <86287753+Ricky-karunakaran@users.noreply.github.com>
2021-12-15 12:44:20 +0800

    Add files via upload

--- wwwroot/js/function.js
+++ wwwroot/js/function.js
@@ -1,9 +1,10 @@
 ﻿let map, infoWindow;//Google Map:global variable
 let page = 1; //Paging:global variable
 
-
+let biggest = 0;
+let distanceList = [];
 //Google Map Function
-function initMap() {
+function initMaps() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 3.8925533126426664, lng: 102.10080302222984 },
         zoom: 8, 
@@ -46,44 +47,142 @@ function initMap() {
         }
     });
 
+
     //setMarker(map);
-    let loopsize = document.getElementsByName('itemLat').length;
-    var images =
+    const images =
     {
         url: "../images/markericon.png",
-        size: new google.maps.Size(71, 71),
-        scaledSize: new google.maps.Size(25, 25),
+        // This marker is 20 pixels wide by 32 pixels high.
+        size: new google.maps.Size(32, 32),
+        // The origin for this image is (0, 0).
+        origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base of the flagpole at (0, 32).
+        anchor: new google.maps.Point(0, 32),
+        scaledSize: new google.maps.Size(25,25)
     };
-    var listOfMarkes = []
-    var listOfInfoWindow=[]
-    for (let i = 0; i < loopsize; i++) {
-        var x = { lat: parseFloat(document.getElementsByName('itemLat')[i].innerHTML), lng: parseFloat(document.getElementsByName('itemLon')[i].innerHTML) };
-        var t = document.getElementsByName('itemName')[i].innerHTML;
-        
-            listOfMarkes.push(new google.maps.Marker(
-                {
-                    position: x,
-                    map,
-                    title: t,
-                    icon: images,
-                }));
-        listOfInfoWindow.push( new google.maps.InfoWindow({
-            content: t,
-        }));
-        listOfMarkes[i].addListener("click", () => {
-            listOfInfoWindow[i].open({
-                anchor: listOfMarkes[i],
-                map,
-                shouldFocus: false,
-            })
-        });
-    }
+    
+    
 
     //Rearrange
     
 
 }
 
+function initMapr() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 3.8925533126426664, lng: 102.10080302222984 },
+        zoom: 8,
+        mapId: 'ca3e1b773e1fd477',
+    });
+    infoWindow = new google.maps.InfoWindow();
+
+    const locationButton = document.createElement("button");
+    //locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    locationButton.innerHTML = "<img src='https://i.ibb.co/FnJMDQp/zerohungerlogotransparent.png' style='margin:0px 0px 0px 0px;' alt='Your Location' width='100' height='100'>";
+    locationButton.style.marginTop = "0px";
+    locationButton.style.padding = '0px 0px 0px 0px';
+
+    locationButton.style.backgroundColor = "transparent";
+    locationButton.style.border = 'unset';
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
+
+    //setMarker(map);
+    const images =
+    {
+        url: "../images/reservedmarker.png",
+        // This marker is 20 pixels wide by 32 pixels high.
+        size: new google.maps.Size(60, 60),
+        // The origin for this image is (0, 0).
+        origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base of the flagpole at (0, 32).
+        anchor: new google.maps.Point(0, 32),
+        scaledSize: new google.maps.Size(25, 25)
+    };
+ 
+        var x = { lat: parseFloat(document.getElementsByName('itemLat')[0].value), lng: parseFloat(document.getElementsByName('itemLon')[0].value) };
+        const marker=new google.maps.Marker(
+            {
+                position: x,
+                map,
+                title: t,
+                icon:images
+            });
+    console.log(x);
+    var t="Your Food Is Here"
+        const ifw=new google.maps.InfoWindow({
+            content: t,
+        });
+        ifw.addListener("click", () => {
+            ifw.open({
+                anchor: marker,
+                map,
+                shouldFocus: false,
+            })
+        });
+    const flocationButton = document.createElement("button");
+    //locationButton.textContent = "Pan to Current Location";
+    flocationButton.classList.add("custom-map-control-button");
+    flocationButton.innerHTML = "<img src='../images/reservedmarker.png' style='margin:10px 10px 10px 10px;' alt='FoodLocation' width='50' height='50'>";
+    flocationButton.style.marginTop = "10px";
+    flocationButton.style.padding = '10px 10px 10px 10px';
+
+    flocationButton.style.backgroundColor = "transparent";
+    flocationButton.style.border = 'unset';
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(flocationButton);
+    flocationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: parseFloat(document.getElementsByName('itemLat')[0].value),
+                        lng: parseFloat(document.getElementsByName('itemLon')[0].value),
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
+
+    //Rearrange
+
+
+}
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(
@@ -102,6 +201,33 @@ function getLatLon() {
                 const pos = {
                     lat: position.coords.latitude,
                     lng: position.coords.longitude,
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(map);
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+}
+
+
+function GoLocation(lat,lon) {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: lat,
+                    lng: lon,
                 };
 
                 infoWindow.setPosition(pos);
@@ -140,19 +266,38 @@ function success(pos) {
             itemLon = parseFloat(itemLon);
             var x = distance(itemLat, itemLon, crd.latitude, crd.longitude);
             x = x.toFixed(2);
-
+            distanceList.push([i, x]);
+            
             document.getElementsByName('itemDistance')[i].innerHTML = x + "km";
             document.getElementsByName('itemDistance')[i].style.fontWeight = "bold";
-            console.log(crd.latitude);
-            console.log(crd.longitude);
-            console.log(x);
             }
     
-    }
+    } sort();
     //SortDistance();
     pageLoading();
 }
+function sort() {
 
+    
+    
+    for (var j = 0; j < distanceList.length; j++) {
+        smallest = j;
+        for (var i = j; i < distanceList.length ; i++) {
+        if (distanceList[i][1] < distanceList[smallest][1]) {
+            smallest = i;
+        }
+
+        }
+        var tem = distanceList[j];
+        
+        distanceList[j] = distanceList[smallest];
+        distanceList[smallest] = tem;
+    }
+    for (x of distanceList) {
+        
+    }
+    
+}
 function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
 }
@@ -180,9 +325,7 @@ function distance(lat1, lon1, lat2, lon2) {
         return dist;
     }
 }
-function SortDistance() {
 
-}
 //End Function Calculate Cookfood distance
 
 //Display promptBox
@@ -218,11 +361,23 @@ function closeRePrompt()
 //Called when html loaded
 function pageLoading() {
     document.getElementById('pPage').disabled = 'true';
-    var x = document.getElementsByName('itemContainer');
+    var x;
     for (var i = 0; i < 4; i++)
     {
-        if (x[i] != null)
-        { x[i].style.display = "flex"; }
+        if (distanceList[i] != null) {
+        if (document.getElementsByName('itemContainer')[distanceList[i][0]] != null)
+        {
+            x = document.getElementsByName('itemContainer')[distanceList[i][0]];
+            x.style.display = "flex";
+            x.style.order = i;
+            }
+        }
+    }
+    if (distanceList.length < 4) {
+        (document.getElementById('nPage')).disabled = true;
+    
+
+    (document.getElementById('pPage')).disabled = true;
     }
 }
 
@@ -232,23 +387,27 @@ function nextPage()
     page = page + 1;
     var n = page;
     var pageSize = 4
-    var x = document.getElementsByName('itemContainer');
+    var x = distanceList;
     
-        for (var i = n * 4; i <=(n + 1) * 4 - 1; i++) {
-            if (x[i - pageSize - 4] != null)
+    for (var i = n * 4; i <= (n + 1) * 4 - 1; i++) {
+        
+            if (document.getElementsByName('itemContainer')[x[i - pageSize - 4][0]] != null)
             {
-                x[i - pageSize-4].style.display = "none";
+                document.getElementsByName('itemContainer')[x[i - pageSize - 4][0]] .style.display = "none";
             }
-            if (x[i - pageSize] != null)
+
+        if (x[i-pageSize]!=null)
             {
-                x[i-pageSize].style.display = "flex";
+                if (document.getElementsByName('itemContainer')[x[i - pageSize][0]] != null) {
+                    document.getElementsByName('itemContainer')[x[i - pageSize][0]].style.display = "flex";
+                }
             }
             
             
             //disable next page button
             if (Math.ceil((x.length / 4)) == page)
             {
-                console.log(Math.ceil((x.length / 4)));
+                
                 (document.getElementById('nPage')).disabled = true;
             }
             
@@ -264,18 +423,22 @@ function nextPage()
 //called when client click previous page
 function previousPage() {
     var n = page;
-    var x= document.getElementsByName('itemContainer');
+    var x = distanceList;
     var pageSize = 4;
     for (var i = n * 4 - 1; i >= ((n - 1) * 4); i--) {
 
-
         if (x[i] != null) {
-        x[i].style.display = 'none';
+            if (document.getElementsByName('itemContainer')[x[i][0]] != null) {
+                document.getElementsByName('itemContainer')[x[i][0]].style.display = 'none';
+
+            }
+        }
+        if (x[i - pageSize] != null) {
+            if (document.getElementsByName('itemContainer')[x[i - pageSize][0]] != null) {
+                document.getElementsByName('itemContainer')[x[i - pageSize][0]].style.display = 'flex';
+            }
+        }
         
-        }
-        if (x[i-pageSize] != null) {
-            x[i - pageSize].style.display = 'flex';
-        }
     }
     page = page - 1;
     //Disable previous page button
@@ -291,4 +454,15 @@ function previousPage() {
 //Google Map Marker
 function setMarker(m) {
    
+}
+function Change(hide, show) {
+    hide.className= "";
+    show.className= "ReservationTitle";
+    console.log("hello");
+    document.getElementById(hide.id + 'Container').style.display = "none";
+    document.getElementById(show.id + 'Container').style.display = "flex";
+    
+
+
+
 }
