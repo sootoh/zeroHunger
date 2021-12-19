93251e30f743947469c3d1373fd3469afc44f0b6
sootoh <65179455+sootoh@users.noreply.github.com>
2021-12-19 19:47:36 +0800

    Update Delivery.cs

--- Model/Delivery.cs
+++ Model/Delivery.cs
@@ -12,21 +12,17 @@ namespace ZeroHunger.Model
         [Key]
         [Display(Name = "Delivery ID")]
         public int DeliveryID { get; set; }
-        [Required]
         [Display(Name = "Delivery Status")]
         public DeliveryStatus DeliveryStatus { get; set; }
-        [Required]
         [DataType(DataType.DateTime)]
         [Display(Name = "Delivery Time")]
         public DateTime? DeliveryTime { get; set; }
         public int? VolunteerID { get; set; }
-        public int ReceiverID { get; set; }
-        
-        [ForeignKey("VolunteerID")]
+        public int? ReceiverID { get; set; }
+        [ForeignKey("Volunteerref")]
         public virtual User? Volunteer { get; set; }
-        [ForeignKey("ReceiverID")]
-        public virtual User Receiver { get; set; }
+        [ForeignKey("Receiverref")]
+        public virtual User? Receiver { get; set; }
         
-        public virtual ICollection<DeliveryItem>? DeliveryItems { get; set; }
     }
 }
