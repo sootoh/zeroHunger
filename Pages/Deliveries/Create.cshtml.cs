f72f8af679d2caa848359ba15217926fd1bcc89c
Lim Sin Jie <84025799+SinJie00@users.noreply.github.com>
2021-12-19 17:34:30 +0800

    Update Create.cshtml.cs

--- Pages/Deliveries/Create.cshtml.cs
+++ Pages/Deliveries/Create.cshtml.cs
@@ -4,42 +4,50 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
 using System.Linq;
-
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ZeroHunger.Pages.Deliveries
 {
-    public class EditModel : PageModel
+    
+    public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
         [BindProperty]
         public Delivery Delivery { set; get; }
+
         public SelectList VolunteerList { get; set; }
+
         public SelectList ReceiverList { get; set; }
-        public EditModel(ApplicationDbContext db)
+        public CreateModel(ApplicationDbContext db)
         {
             _db = db;
         }
-        public async Task OnGet(int id)
+        public async Task OnGetAsync()
         {
             var items = await _db.User.Where(u => u.UserType.TypeID == 2).ToListAsync();
             VolunteerList = new SelectList(items, "UserID", "UserName");
-            var bitems = await _db.User.Where(u => u.UserType.TypeID == 3).ToListAsync(); ;
-            ReceiverList = new SelectList(bitems, "UserID", "UserName");
+            
+            var ritems = await _db.User.Where(u => u.UserType.TypeID == 3).ToListAsync();
+            ReceiverList = new SelectList(ritems, "UserID", "UserName");
 
-            Delivery = _db.Delivery.Find(id);
         }
         public async Task<IActionResult> OnPost()
         {
+            if(Delivery.DeliveryStatus != 0)
+            {
+                ModelState.AddModelError("Delivery.DeliveryStatus", "The delivery status must be pending now.");
+            }
             if (ModelState.IsValid)
             {
-                _db.Delivery.Update(Delivery);
+                await _db.Delivery.AddAsync(Delivery);
                 await _db.SaveChangesAsync();
-                TempData["success"] = "Delivery request updated successfully";
+                TempData["success"] = "Delivery request created successfully";
                 return RedirectToPage("Index");
             }
             return Page();
         }
+
     }
 }
