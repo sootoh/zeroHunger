cfbc827b80df4ca42713c2616f8a1a1c19bd1fa5
Lim Sin Jie <84025799+SinJie00@users.noreply.github.com>
2021-12-19 17:47:19 +0800

    Update Delivery.cs

--- Model/Delivery.cs
+++ Model/Delivery.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using ZeroHunger.Model;
@@ -12,17 +12,21 @@ namespace ZeroHunger.Model
         [Key]
         [Display(Name = "Delivery ID")]
         public int DeliveryID { get; set; }
+        [Required]
         [Display(Name = "Delivery Status")]
         public DeliveryStatus DeliveryStatus { get; set; }
+        [Required]
         [DataType(DataType.DateTime)]
         [Display(Name = "Delivery Time")]
         public DateTime? DeliveryTime { get; set; }
         public int? VolunteerID { get; set; }
-        public int? ReceiverID { get; set; }
+        public int ReceiverID { get; set; }
+        
         [ForeignKey("VolunteerID")]
         public virtual User? Volunteer { get; set; }
         [ForeignKey("ReceiverID")]
-        public virtual User? Receiver { get; set; }
+        public virtual User Receiver { get; set; }
+        
         public virtual ICollection<DeliveryItem>? DeliveryItems { get; set; }
     }
 }
