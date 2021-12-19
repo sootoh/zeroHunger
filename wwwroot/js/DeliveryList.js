7d11d6aeb38beeec6d13e10365ef70ad2389e74c
sootoh <65179455+sootoh@users.noreply.github.com>
2021-12-19 19:49:45 +0800

    Update DeliveryList.js

--- wwwroot/js/DeliveryList.js
+++ wwwroot/js/DeliveryList.js
@@ -1,11 +1,11 @@
-﻿var dataTable;
+var dataTable;
 
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    dataTable = $('#DT_load').DataTable({
+    dataTable = $('#delivery_load').DataTable({
         "ajax": {
             "url": "/api/delivery",
             "type": "GET",
@@ -16,14 +16,14 @@ function loadDataTable() {
                 for (var i = 0, ien = json.data.length; i < ien; i++) {
                     json.data[i][0] = '<a href="/message/' + json.data[i][0] + '>View message</a>';
                 },*/
-            { "data": "volunteer.UserName", "width": "5%" },
+            { "data": "volunteer.userName","width": "5%" },
             { "data": "deliveryTime", "width": "10%" },
             { "data": "deliveryStatus", "width": "2%" },
-            { "data": "Receiver.userName", "width": "5%" },
-            { "data": "Receiver.userPhone", "width": "5%" },
-            { "data": "Receiver.userAdrs1 Receiver.userAdrs2", "width": "15%" },
+            { "data": "receiver.userName", "width": "5%" },
+            { "data": "receiver.userPhone", "width": "5%" },
+            { "data": "receiver.userAdrs1", "width": "15%" },
             {
-                "data": "deliveryid",
+                "data": "deliveryID",
                 "render": function (data) {
                     return `<td>
                         <a href="/Deliveries/DeliveryItem?id=${data}" class='btn btn-info text-white' style='cursor:pointer; width:70px;'>
@@ -33,7 +33,7 @@ function loadDataTable() {
                 }, "width": "10%"
             },
             {
-                "data": "deliveryid",
+                "data": "deliveryID",
                 "render": function (data) {
                     return `<td>
                         <a href="/Deliveries/Edit?id=${data}" class="btn btn-success mx-2 text-white" style="cursor:pointer;width:80px">
@@ -43,10 +43,10 @@ function loadDataTable() {
                 }, "width": "10%"
             },
             {
-                "data": "deliveryid",
+                "data": "deliveryID",
                 "render": function (data) {
                     return `<td>
-                        <a href="/api/delivery?id=${data}" class="btn btn-danger mx-2 text-white" style="cursor:pointer;width:100px">
+                        <a onclick="/api/delivery?id=${data}" class="btn btn-danger mx-2 text-white" style="cursor:pointer;width:100px">
                         Delete &nbsp;<i class="bi bi-trash-fill"></i>
                         </a>
                         </td>`;
