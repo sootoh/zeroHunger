3a101eb8adba28477da3a9256f1d3769bf4dc96c
sootoh <65179455+sootoh@users.noreply.github.com>
2021-12-31 22:46:19 +0800

    Update ForgotPassword.cshtml.cs

--- Pages/ForgotPassword.cshtml.cs
+++ Pages/ForgotPassword.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
@@ -6,11 +5,15 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
 using ZeroHunger.Data;
 using ZeroHunger.Model;
+using SocketLabs.InjectionApi;
+using SocketLabs.InjectionApi.Message;
+
 
 namespace ZeroHunger.Pages
 {
@@ -31,27 +34,47 @@ namespace ZeroHunger.Pages
            
             
         }
-        public static void SendEmail(string emailbody, string userEmail)
-        {
-            // Specify the from and to email address
-            MailMessage mailMessage = new MailMessage("vtechzerohunger@gmail.com", userEmail);
-            // Specify the email body
-            mailMessage.Body = emailbody;
-            // Specify the email Subject
-            mailMessage.Subject = "Reset Password Zero Hunger";
+        //public static void SendEmail(string emailbody, string userEmail)
+        //{
+            
+        //    // Specify the from and to email address
+        //    MailMessage mailMessage = new MailMessage("vtechzerohunger@gmail.com", userEmail);
+        //    // Specify the email body
+        //    mailMessage.Body = emailbody;
+        //    // Specify the email Subject
+        //    mailMessage.Subject = "Reset Password Zero Hunger";
 
-            // Specify the SMTP server name and post number
-            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
-            // Specify your gmail address and password
-            smtpClient.Credentials = new System.Net.NetworkCredential()
-            {
-                UserName = "vtechzerohunger@gmail.com",
-                Password = "ad_0hunger"
-            };
-            // Gmail works on SSL, so set this property to true
-            smtpClient.EnableSsl = true;
-            // Finall send the email message using Send() method
-            smtpClient.Send(mailMessage);
+        //    // Specify the SMTP server name and post number
+        //    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+        //    // Specify your gmail address and password
+        //    smtpClient.Credentials = new System.Net.NetworkCredential()
+        //    {
+        //        UserName = "vtechzerohunger@gmail.com",
+        //        Password = "ad_0hunger"
+        //    };
+        //    // Gmail works on SSL, so set this property to true
+        //    smtpClient.EnableSsl = true;
+        //    // Finall send the email message using Send() method
+        //    smtpClient.Send(mailMessage);
+        //}
+
+        public void anotherSendEmail(string emailbody, string userEmail)
+        {
+
+            var client = new SocketLabsClient(42290, "e5N6HwBb2k8A3Mrc9R7K"); //Your SocketLabs ServerId and Injection API key
+
+            var message = new BasicMessage();
+
+            message.Subject = "Zero Hunger Reset Password";
+            message.HtmlBody = emailbody;
+            message.PlainTextBody = emailbody;
+
+            message.From.Email = "vtechzerohunger@gmail.com";
+
+            //A basic message supports up to 50 recipients and supports several different ways to add recipients
+            message.To.Add(userEmail); //Add a To address by passing the email address
+
+            var response = client.Send(message);
         }
         public IActionResult OnPost(string userEmail)
         {
@@ -68,7 +91,7 @@ namespace ZeroHunger.Pages
                     //new { email = model.userEmail, token = rnd }, Request.Scheme);
                     resetPasswordLink = "https://localhost:44306/ResetPassword?UserID=" + forgotUser.UserID;
 
-                    SendEmail(("Please reset your password \\" + resetPasswordLink), userEmail);
+                    anotherSendEmail(("Please reset your password \n" + resetPasswordLink), userEmail);
                     ViewData["Message"] = string.Format("We have sent an email to you.\\Please check your email.");
 
 
