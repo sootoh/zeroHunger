69d41c9bf5a83e352527773939ae1ebb1d9a7bf3
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2022-01-06 02:06:04 +0800

    Add files via upload

--- Pages/ReceiverAccountApplication/ViewReceiverAccountApplication.cshtml.cs
+++ Pages/ReceiverAccountApplication/ViewReceiverAccountApplication.cshtml.cs
@@ -21,16 +21,18 @@ namespace ZeroHunger.Pages
         public Receiver application { get; set; }
         public IList<ReceiverFamily> family { get; set; } = new List<ReceiverFamily>();
         public IEnumerable<SalaryGroup> salaryGroups { get; set; }
+        public ReceiverQuestionnaire questionnaire { get; set; }
         public async Task OnGetAsync(string? id)
         {
             application=new Receiver();
+            questionnaire=new ReceiverQuestionnaire();
             salaryGroups = _db.SalaryGroup;
 
             if (id!=null)
             {
 
                 application = _db.Receiver.Where(i => i.receiverIC.Equals(id)).Single();
-                
+                questionnaire=_db.ReceiverQuestionnaire.Where(i=> i.receiverIC.Equals(id)).OrderBy(j=>j.questionnaireId).LastOrDefault();
 
                 foreach(ReceiverFamily familyInfo in _db.ReceiverFamily)
                 {
