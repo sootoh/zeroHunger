8856332c276a37488d6516ad6f28d979864a82b0
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2022-01-08 01:54:03 +0800

    Add files via upload

---
+++ Controllers/ReceiverController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using ZeroHunger.Data;
+using ZeroHunger.Model;
+
+namespace ZeroHunger.Controllers
+{
+    [Route("api/ReceiverList")]
+    [ApiController]
+    public class ReceiverController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ReceiverController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            this._hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public IList<Receiver> receiverList { get; set; } = new List<Receiver>();
+        public IList<ReceiverQuestionnaire> questionnaireList { get; set; } = new List<ReceiverQuestionnaire>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            foreach (Receiver receiver in _db.Receiver)
+            {
+                ReceiverQuestionnaire questionnaire=new ReceiverQuestionnaire();
+                if (receiver.applicationStatusID >= 2)
+                {
+                    receiver.receiverSalaryGroup = _db.SalaryGroup.Where(i => i.salaryGroupID.Equals(receiver.receiverSalaryGroupID)).Single();
+                    questionnaire = _db.ReceiverQuestionnaire.Where(i => i.receiverIC.Equals(receiver.receiverIC)).OrderBy(j => j.questionnaireId).LastOrDefault();
+                    if(questionnaire != null)
+                    {
+                        questionnaire.receiver=receiver;
+                        questionnaireList.Add(questionnaire);
+                    }
+                    else
+                    {
+                        questionnaire=new ReceiverQuestionnaire();
+                        questionnaire.receiver = receiver;
+                        questionnaireList.Add(questionnaire);
+                    }
+                    
+                }
+
+            }
+            
+            return Json(new { data = questionnaireList });
+        }
+    }
+}
