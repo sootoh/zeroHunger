2dfa3d3803bdee4d63d26b53a2ebdb76e1313497
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2022-01-08 01:49:16 +0800

    Add files via upload

---
+++ wwwroot/js/receiverList.js
@@ -0,0 +1,60 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/receiverlist",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "receiver.receiverIC" },
+            { "data": "receiver.receiverName" },
+            { "data": "receiver.receiverOccupation" },
+            {
+                "data": "receiver.receiverSalaryGroup.salaryRange"
+                //"data": "receiverSalaryGroupID",
+                //"render": function (data) {
+                //    return `@Html.DisplayFor(SalaryGroup => Model.salaryGroups.ElementAt(${data}).salaryRange)`
+                //}
+            },
+
+            { "data": "receiver.receiverFamilyNo" },
+            {
+                "data": "receiver.receiverAdrs1",
+                "render": function (data, type, row) {
+                    return data + '<br>' + row.receiver.receiverAdrs2;
+                }
+            },
+            {
+                "data": "receiver.applicationStatusID",
+                "render": function (data) {
+                    if (data == 2) {
+                        return "Seen";
+                    }
+                    else if (data == 1) {
+                        return "New Updates!";
+                    }
+                }
+            },
+            { "data": "date" },
+            {
+                "data": "receiver.receiverIC",
+                "render": function (data) {
+                    return `
+                        <a href="/ReceiverList/ViewReceiver?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
+                            Details
+                        </a>`;
+                }
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
