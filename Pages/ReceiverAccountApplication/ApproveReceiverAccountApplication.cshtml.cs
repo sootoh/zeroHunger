2afa4acbc6ede3f178051b20dda8074884a4825b
yilin6037 <65179589+yilin6037@users.noreply.github.com>
2022-01-09 00:24:24 +0800

    Add files via upload

--- Pages/ReceiverAccountApplication/ApproveReceiverAccountApplication.cshtml.cs
+++ Pages/ReceiverAccountApplication/ApproveReceiverAccountApplication.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SocketLabs.InjectionApi;
+using SocketLabs.InjectionApi.Message;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -21,27 +23,45 @@ namespace ZeroHunger.Pages
         public string message { get; set; } = "";
         public string emailBody { get; set; } = "";
 
-        public static void SendEmail(string emailbody, string userEmail)
-        {
-            // Specify the from and to email address
-            MailMessage mailMessage = new MailMessage("vtechzerohunger@gmail.com", userEmail);
-            // Specify the email body
-            mailMessage.Body = emailbody;
-            // Specify the email Subject
-            mailMessage.Subject = "Your application is approved!";
+        //public static void SendEmail(string emailbody, string userEmail)
+        //{
+        //    // Specify the from and to email address
+        //    MailMessage mailMessage = new MailMessage("vtechzerohunger@gmail.com", userEmail);
+        //    // Specify the email body
+        //    mailMessage.Body = emailbody;
+        //    // Specify the email Subject
+        //    mailMessage.Subject = "Your application is approved!";
 
-            // Specify the SMTP server name and post number
-            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
-            // Specify your gmail address and password
-            smtpClient.Credentials = new System.Net.NetworkCredential()
-            {
-                UserName = "vtechzerohunger@gmail.com",
-                Password = "ad_0hunger"
-            };
-            // Gmail works on SSL, so set this property to true
-            smtpClient.EnableSsl = true;
-            // Finall send the email message using Send() method
-            smtpClient.Send(mailMessage);
+        //    // Specify the SMTP server name and post number
+        //    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+        //    // Specify your gmail address and password
+        //    smtpClient.Credentials = new System.Net.NetworkCredential()
+        //    {
+        //        UserName = "vtechzerohunger@gmail.com",
+        //        Password = "ad_0hunger"
+        //    };
+        //    // Gmail works on SSL, so set this property to true
+        //    smtpClient.EnableSsl = true;
+        //    // Finall send the email message using Send() method
+        //    smtpClient.Send(mailMessage);
+        //}
+        public void anotherSendEmail(string emailbody, string userEmail)
+        {
+
+            var client = new SocketLabsClient(42290, "e5N6HwBb2k8A3Mrc9R7K"); //Your SocketLabs ServerId and Injection API key
+
+            var message = new BasicMessage();
+
+            message.Subject = "Your application is approved!";
+            message.HtmlBody = emailbody;
+            message.PlainTextBody = emailbody;
+
+            message.From.Email = "vtechzerohunger@gmail.com";
+
+            //A basic message supports up to 50 recipients and supports several different ways to add recipients
+            message.To.Add(userEmail); //Add a To address by passing the email address
+
+            var response = client.Send(message);
         }
         public async Task<IActionResult> OnGetAsync(string? id)
         {
@@ -74,14 +94,14 @@ namespace ZeroHunger.Pages
 
                 await _db.SaveChangesAsync();
 
-                /*emailBody = "We appreciate you taking the time to apply as a receiver to ZeroHunger. "
-                    + "We are pleased to tell you that your application is approved.\\"
-                    + "Please login to your account with the following information:\\"
+                emailBody = "We appreciate you taking the time to apply as a receiver to ZeroHunger. "
+                    + "We are pleased to tell you that your application is approved.\n"
+                    + "Please login to your account with the following information:\n"
                     + "Email: " + application.receiverEmail
-                    + "\\Password: 123"
-                    + "\\https://localhost:44306/login";
+                    + "\nPassword: 123"
+                    + "\nhttps://zerohunger20211219221449.azurewebsites.net/login" ;
 
-                SendEmail(emailBody, application.receiverEmail);*/
+                anotherSendEmail(emailBody, application.receiverEmail);
 
                 message = "The application is approved. An email is sent to the applicant's e-mail address. " +
                     "email: " + application.receiverEmail + " " +
@@ -92,7 +112,7 @@ namespace ZeroHunger.Pages
                 return RedirectToPage("Index");
             }
             message = "Error!";
-            return RedirectToPage();
+            return RedirectToPage("ViewReceiverAccountApplication");
 
         }
     }
--- Pages/ReceiverAccountApplication/RejectReceiverAccountApplication.cshtml.cs
+++ Pages/ReceiverAccountApplication/RejectReceiverAccountApplication.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SocketLabs.InjectionApi;
+using SocketLabs.InjectionApi.Message;
 using System.Linq;
 using System.Net.Mail;
 using System.Threading.Tasks;
@@ -19,27 +21,45 @@ namespace ZeroHunger.Pages.ReceiverAccountApplication
         public Receiver application { get; set; }
         public string message { get; set; } = "";
 
-        public static void SendEmail(string emailbody, string userEmail)
-        {
-            // Specify the from and to email address
-            MailMessage mailMessage = new MailMessage("vtechzerohunger@gmail.com", userEmail);
-            // Specify the email body
-            mailMessage.Body = emailbody;
-            // Specify the email Subject
-            mailMessage.Subject = "Sorry! Your application is rejected!";
+        //public static void SendEmail(string emailbody, string userEmail)
+        //{
+        //    // Specify the from and to email address
+        //    MailMessage mailMessage = new MailMessage("vtechzerohunger@gmail.com", userEmail);
+        //    // Specify the email body
+        //    mailMessage.Body = emailbody;
+        //    // Specify the email Subject
+        //    mailMessage.Subject = "Sorry! Your application is rejected!";
 
-            // Specify the SMTP server name and post number
-            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
-            // Specify your gmail address and password
-            smtpClient.Credentials = new System.Net.NetworkCredential()
-            {
-                UserName = "vtechzerohunger@gmail.com",
-                Password = "ad_0hunger"
-            };
-            // Gmail works on SSL, so set this property to true
-            smtpClient.EnableSsl = true;
-            // Finall send the email message using Send() method
-            smtpClient.Send(mailMessage);
+        //    // Specify the SMTP server name and post number
+        //    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+        //    // Specify your gmail address and password
+        //    smtpClient.Credentials = new System.Net.NetworkCredential()
+        //    {
+        //        UserName = "vtechzerohunger@gmail.com",
+        //        Password = "ad_0hunger"
+        //    };
+        //    // Gmail works on SSL, so set this property to true
+        //    smtpClient.EnableSsl = true;
+        //    // Finall send the email message using Send() method
+        //    smtpClient.Send(mailMessage);
+        //}
+        public void anotherSendEmail(string emailbody, string userEmail)
+        {
+
+            var client = new SocketLabsClient(42290, "e5N6HwBb2k8A3Mrc9R7K"); //Your SocketLabs ServerId and Injection API key
+
+            var message = new BasicMessage();
+
+            message.Subject = "Sorry! Your application is rejected!";
+            message.HtmlBody = emailbody;
+            message.PlainTextBody = emailbody;
+
+            message.From.Email = "vtechzerohunger@gmail.com";
+
+            //A basic message supports up to 50 recipients and supports several different ways to add recipients
+            message.To.Add(userEmail); //Add a To address by passing the email address
+
+            var response = client.Send(message);
         }
 
         public async Task<IActionResult> OnGetAsync(string? id)
@@ -61,10 +81,10 @@ namespace ZeroHunger.Pages.ReceiverAccountApplication
 
                 await _db.SaveChangesAsync();
 
-                /*string emailBody = "We appreciate you taking the time to apply as a receiver to ZeroHunger. "
+                string emailBody = "We appreciate you taking the time to apply as a receiver to ZeroHunger. "
                     + "However, in order to make the best use of the resources available to us, we had to make the painful decision to refuse your application.";
 
-                SendEmail(emailBody, email);*/
+                anotherSendEmail(emailBody, email);
 
                 message = "The application is removed. An email is sent to the applicant's e-mail address.";
                 TempData["alertMessage"] = "The application is approved. An email is sent to the applicant." +
@@ -72,7 +92,7 @@ namespace ZeroHunger.Pages.ReceiverAccountApplication
                 return RedirectToPage("Index");
             }
             message = "Error!";
-            return RedirectToPage();
+            return RedirectToPage("ViewReceiverAccountApplication");
 
         }
     }
