8126642d9b7d0cb9262fbf69a0b8c140951d7fdf
sootoh <65179455+sootoh@users.noreply.github.com>
2022-01-11 03:11:21 +0800

    Update ForgotPassword.cshtml.cs

--- Pages/ForgotPassword.cshtml.cs
+++ Pages/ForgotPassword.cshtml.cs
@@ -34,34 +34,11 @@ namespace ZeroHunger.Pages
            
             
         }
-        //public static void SendEmail(string emailbody, string userEmail)
-        //{
-            
-        //    // Specify the from and to email address
-        //    MailMessage mailMessage = new MailMessage("vtechzerohunger@gmail.com", userEmail);
-        //    // Specify the email body
-        //    mailMessage.Body = emailbody;
-        //    // Specify the email Subject
-        //    mailMessage.Subject = "Reset Password Zero Hunger";
-
-        //    // Specify the SMTP server name and post number
-        //    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
-        //    // Specify your gmail address and password
-        //    smtpClient.Credentials = new System.Net.NetworkCredential()
-        //    {
-        //        UserName = "vtechzerohunger@gmail.com",
-        //        Password = "ad_0hunger"
-        //    };
-        //    // Gmail works on SSL, so set this property to true
-        //    smtpClient.EnableSsl = true;
-        //    // Finall send the email message using Send() method
-        //    smtpClient.Send(mailMessage);
-        //}
 
         public void anotherSendEmail(string emailbody, string userEmail)
         {
 
-            var client = new SocketLabsClient(42290, "e5N6HwBb2k8A3Mrc9R7K"); //Your SocketLabs ServerId and Injection API key
+            var client = new SocketLabsClient(42290, "e5N6HwBb2k8A3Mrc9R7K"); 
 
             var message = new BasicMessage();
 
@@ -71,27 +48,23 @@ namespace ZeroHunger.Pages
 
             message.From.Email = "vtechzerohunger@gmail.com";
 
-            //A basic message supports up to 50 recipients and supports several different ways to add recipients
-            message.To.Add(userEmail); //Add a To address by passing the email address
+            
+            message.To.Add(userEmail); 
 
             var response = client.Send(message);
         }
         public IActionResult OnPost(string userEmail)
         {
             
-                // Find the user by email
+                
                 forgotUser = (User)(_db.User.Where(b => b.UserEmail.Equals(userEmail)).FirstOrDefault());
-                // If the user is found AND Email is confirmed
+                
                 if (forgotUser != null)
                 {
 
-                    //var token = await Microsoft.AspNetCore.Identity.UserManager<User>.GeneratePasswordResetTokenAsync(forgotUser);
-                    // Build the password reset link
-                    //string passwordResetLink = Url.Action("ResetPassword", "Account",
-                    //new { email = model.userEmail, token = rnd }, Request.Scheme);
-                    resetPasswordLink = "https://localhost:44306/ResetPassword?UserID=" + forgotUser.UserID;
+                    resetPasswordLink = "https://zerohunger20211219221449.azurewebsites.net/ResetPassword?p=" + forgotUser.UserID;
 
-                    anotherSendEmail(("Please reset your password \n" + resetPasswordLink), userEmail);
+                    anotherSendEmail(("Please reset your password <br>" + resetPasswordLink), userEmail);
                     ViewData["Message"] = string.Format("We have sent an email to you.\\Please check your email.");
 
 
