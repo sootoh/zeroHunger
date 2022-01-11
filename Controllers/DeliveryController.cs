9b3fb52ddb9074a30a8a71eade602d37f0f19f64
Lim Sin Jie <84025799+SinJie00@users.noreply.github.com>
2022-01-11 19:43:31 +0800

    Update DeliveryController.cs

--- Controllers/DeliveryController.cs
+++ Controllers/DeliveryController.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 using ZeroHunger.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using ZeroHunger.Model;
 using Microsoft.AspNetCore.Hosting;
-using System.IO;
-using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Diagnostics;
-using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace ZeroHunger.Controllers
 {
@@ -32,46 +27,79 @@ namespace ZeroHunger.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            /*if (@User.Identity.Name != null)
+            deliveries = await _db.Delivery.OrderByDescending(d => d.DeliveryTime).ToListAsync();
+            int i = 1;
+            foreach (var delivery in deliveries)
             {
-                loginUser = (_db.User.Where(b => b.UserEmail.Equals(@User.Identity.Name)).FirstOrDefault());
-                return Json(new { data = await _db.Delivery.Where(c => c.DonorUserID.Equals(loginUser.UserID)).ToListAsync() });
-
+                if (delivery.DeliveryStatus.Equals(4))
+                {
+                    var id = delivery.DeliveryID.ToString();
+                    ViewData["Message" + i.ToString()] = "Delivery with ID " + id + " is rejected. Please find a new volunteer.";
+                    i++;
+                }
             }
-            else
-            {*/
-            //var volunteername =  _db.Delivery.Include(d => d.Receiver).ToListAsync(); , volunteer = volunteername 
-
-            deliveries = await _db.Delivery.ToListAsync();
-            //int.TryParse(HttpContext.Session.GetString("userid"), out uid);
-            //CR = await _db.CookReservation.Where(b => b.userId.Equals(uid)).ToListAsync();
+            foreach (var delivery in deliveries)
+            {
+                if (_db.DeliveryItem.Find(delivery.DeliveryID) == null)
+                {
+                    var delid = delivery.DeliveryID.ToString();
+                    ViewData["Message" + i.ToString()] = "Delivery with ID " + delid + " has no delivery item. Please add delivery item now.";
+                    i++;
+                }
+            }
             foreach (var item in deliveries)
             {
-
                 item.Volunteer = await _db.User.FindAsync(item.VolunteerID);
                 item.Receiver = await _db.User.FindAsync(item.ReceiverID);
             }
-            
             return Json(new { data = deliveries });
-
         }
-
+        /*
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CheckDeliveryItem_Request()
+        {
+           deliveries = await _db.Delivery.OrderByDescending(d => d.DeliveryTime).ToListAsync();
+            int i = 1;
+            foreach(var delivery in deliveries)
+            {
+                if (delivery.DeliveryStatus.Equals(4))
+                {
+                    var id = delivery.DeliveryID.ToString();
+                    ViewData["Message" + i.ToString()] = "Delivery with ID " + id + " is rejected. Please find a new volunteer.";
+                    i++;
+                }
+            }
+            foreach(var delivery in deliveries)
+            {
+                if (_db.DeliveryItem.Find(delivery.DeliveryID) == null)
+                {
+                    var delid = delivery.DeliveryID.ToString();
+                    ViewData["Message" + i.ToString()] = "Delivery with ID " + delid + " has no delivery item. Please add delivery item now.";
+                    i++;
+                }
+            }
+            return View();
+        }*/
+        
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
         {
-            var deliveryFromDb = _db.Delivery.SingleOrDefaultAsync(d => d.DeliveryID == id);
+            var deliveryFromDb = await _db.Delivery.FirstOrDefaultAsync(d => d.DeliveryID == id);
             if (deliveryFromDb == null)
             {
                 return Json(new { success = false, message = "Error while Deleting" });
             }
-            List<DeliveryItem> itemlist = await _db.DeliveryItem.Where(d=>d.DeliveryID == id).ToListAsync();
+            /*List<DeliveryItem> itemlist = await _db.DeliveryItem.Where(d=>d.DeliveryID == id).ToListAsync();
             foreach(var item in itemlist)
             {
                 _db.DeliveryItem.Remove(item);
-            }
-            _db.Delivery.Remove(await deliveryFromDb);
+            }*/
+            List<DeliveryItem> itemList = await _db.DeliveryItem.ToListAsync();
+            itemList.RemoveAll(i => i.DeliveryID == id);
+            _db.Delivery.Remove(deliveryFromDb);
             await _db.SaveChangesAsync();
-            return Json(new { success = true, message = "Delete successful" });
+            return Json(new { success = true, message = "Delete Successful" });
         }
     }
 }
