2768f25c5ab75a4576ee429003a3bd2728014af9
sootoh <65179455+sootoh@users.noreply.github.com>
2022-01-11 18:00:38 +0800

    Update ResetPassword.cshtml.cs

--- Pages/ResetPassword.cshtml.cs
+++ Pages/ResetPassword.cshtml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 using System.Threading.Tasks;
 using ZeroHunger.Data;
 using ZeroHunger.Model;
@@ -11,8 +12,9 @@ namespace ZeroHunger.Pages
 {
     public class ResetPasswordModel : PageModel
     {
+        [BindProperty]
         public string password{ get; set; } = "";
-        public string userEmail { get; set; } = "";
+        public int id { get; set; }
         private readonly ApplicationDbContext _db;
         [BindProperty]
         public User forgotUser { get; set; }
@@ -20,22 +22,32 @@ namespace ZeroHunger.Pages
         {
             _db = db;
         }
-        public async Task OnGetAsync(int id)
+        public async Task OnGetAsync(string p,string n, string a)
         {
+            id = int.Parse(p)+87-777;
             forgotUser = await _db.User.FindAsync(id);
         }
 
-        public async Task<IActionResult> OnPostAsync(string password)
+        public async Task<IActionResult> OnPostAsync(string password, int id)
         {
-            forgotUser = await _db.User.FindAsync(forgotUser.UserID);
-
-            if (forgotUser != null)
+            //forgotUser = await _db.User.FindAsync(forgotUser.UserID);
+            User resetPwdUser = _db.User.Where(b => b.UserID.Equals(id)).FirstOrDefault();
+            if (resetPwdUser != null)
             {
-                forgotUser.UserPwd = password;
+                resetPwdUser.UserPwd = password;
                 await _db.SaveChangesAsync();
-                ViewData["Message"] = string.Format("You password was updated.\\Please try to login.");
-                return RedirectToPage("login");
-                
+                if (@User.Identity.IsAuthenticated)
+                {
+                    ViewData["Message"] = string.Format("You password was updated.");
+
+                    return RedirectToPage("Index");
+                }
+                else
+                {
+                    ViewData["Message"] = string.Format("You password was updated.\\Please try to login.");
+
+                    return RedirectToPage("login");
+                }
             }
             else
             {
