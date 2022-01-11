de1ccdea80da048b387b4fba7a17c814d4a7f58f
TANCT <86305039+TANCT@users.noreply.github.com>
2022-01-12 00:57:47 +0800

    Update PIDList.js

--- wwwroot/js/PIDList.js
+++ wwwroot/js/PIDList.js
@@ -54,11 +54,11 @@ function Delete(url) {
                 url: url,
                 success: function (data) {
                     if (data.success) {
-                        toastr.success(data.message);
+                       
                         dataTable.ajax.reload();
                     }
                     else {
-                        toastr.error(data.message);
+                        
                     }
                 }
             });
