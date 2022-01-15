0952d59a45ae83fc3e3ce1ba76135010cf26b115
RICKY <86287753+Ricky-karunakaran@users.noreply.github.com>
2022-01-15 19:51:58 +0800

    Add files via upload

--- Pages/DryFoodDonationF/AddDryFoodDonation.cshtml.cs
+++ Pages/DryFoodDonationF/AddDryFoodDonation.cshtml.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SocketLabs.InjectionApi;
+using SocketLabs.InjectionApi.Message;
 using ZeroHunger.Data;
 using ZeroHunger.Model;
 
@@ -20,6 +23,7 @@ namespace ZeroHunger.Pages.DryFoodDonationF
         }
         [BindProperty]
         public DryFoodDonation DFD { get; set; }
+        public IEnumerable<User> Admin { get; set; }
         public async Task<IActionResult> OnGet()
         {
             string uids = HttpContext.Session.GetString("userid");
@@ -45,6 +49,21 @@ namespace ZeroHunger.Pages.DryFoodDonationF
                 dfd.donor_Id = _db.User.Find(dfd.donorid);
                 await _db.DryFoodDonation.AddAsync(dfd);
                 await _db.SaveChangesAsync();
+                Admin = await _db.User.Where(b => b.TypeId==4).ToListAsync();
+                string message = "<p style='font-size:20px;'> <b style='font-size:x-large;'> Dry Food Donation Received</b><br/>";
+                string dn = "<b>Name:</b> " + dfd.donor_Id.UserName + "<br/>";
+                string dp = "<b>Phone:</b> " + dfd.donor_Id.UserPhone + "<br/>";
+                string item = "<b>Item:</b> " + dfd.DryFoodName + "<br/>";
+                string q = "<b>Quantity:</b>  " + dfd.DryFoodQuantity + "<br/>";
+                string dm = "<b>Delivery Method:</b> <b style='color:red'>" + dfd.DeliveryMethod + "</b><br/>";
+                string rm = "<b>Remark:</b>  " + dfd.DryFoodRemark + "<br/>";
+                string pd = "<b>Pick up date:</b><b style='color:red'> " + dfd.DryFoodPickDate + "</b><br/>";
+                string footer = "<br/><br/>Thanks,<br/>Zero Hunger<br/><b>This a computer auto-generated email, do not reply to this email.</b></p>";
+                foreach(var adminitem in Admin)
+                {
+                    anotherSendEmail((message + dn + dp + item + pd + dm + q  + rm + footer),adminitem.UserEmail);
+                }
+                
                 return RedirectToPage("DryFoodIndex");
                 
             }
@@ -52,6 +71,24 @@ namespace ZeroHunger.Pages.DryFoodDonationF
             {
                 return Page();
             }
+        }
+        public void anotherSendEmail(string emailbody, string userEmail)
+        {
+            //userEmail = "ricky.k@graduate.utm.my";
+            var client = new SocketLabsClient(42290, "e5N6HwBb2k8A3Mrc9R7K"); //Your SocketLabs ServerId and Injection API key
+
+            var message = new BasicMessage();
+
+            message.Subject = "Donation Received";
+            message.HtmlBody = emailbody;
+            message.PlainTextBody = emailbody;
+
+            message.From.Email = "vtechzerohunger@gmail.com";
+
+            //A basic message supports up to 50 recipients and supports several different ways to add recipients
+            message.To.Add(userEmail); //Add a To address by passing the email address
+
+            var response = client.Send(message);
         }
     }
 }
