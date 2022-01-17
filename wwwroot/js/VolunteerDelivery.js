b38a175d7780b868d2c2c55317608115005e0487
sootoh <65179455+sootoh@users.noreply.github.com>
2022-01-17 11:03:07 +0800

    Add files via upload

---
+++ wwwroot/js/VolunteerDelivery.js
@@ -0,0 +1,166 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#volunteer_load').DataTable({
+        "ajax": {
+            "url": "/api/volunteerdelivery",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "deliveryID", "width": "2%" },
+            {
+                "data": "receiver.userName",
+                "render": function (data) {
+                    if (data == null) {
+                        return 'Deleted User';
+                    }
+                    else {
+                        return data;
+                    }
+                },"width": "5%"
+            },
+            {
+                "data": "receiver.userPhone",
+                "render": function (data) {
+                    if (data == null) {
+                        return 'Empty data';
+                    }
+                    else {
+                        return data;
+                    }
+                },"width": "5%"
+            },
+            {
+                "data": "receiver.userAdrs1",
+                "render": function (data, type, row) {
+                    if (data == null) {
+                        return 'Empty data';
+                    }
+                    else {
+                        return data + ' ' + row.receiver.userAdrs2;
+                    }
+                }, "width": "15%"
+            },
+            { "data": "deliveryTime", "width": "15%" },
+            {
+                "data": "deliveryStatus",
+                "render": function (data) {
+                    var status;
+                    if (data == 0) {
+                        status = "pending";
+                    }
+                    else if (data == 1) {
+                        status = "incomplete";
+                    }
+                    else if (data == 2) {
+                        status = "complete";
+                    }
+                    else if (data == 3) {
+                        status = "rejected";
+                    }
+                    return status;
+                }, "width": "2%"
+            },
+            {
+                "data": "deliveryID",
+                "render": function (data) {
+                    return `
+                        <a href="/Deliveries/ViewDeliveryItem?id=${data}" class='btn btn-info text-white' style='cursor:pointer; width:100px;'>
+                            View 
+                        </a>`;
+                }, "width": "10%"
+            },
+            {
+                "data": "deliveryStatus",
+                "render": function (data, type, row) {
+                    if (data == 0) {
+                        return `<td>
+                         <a class='btn btn-success text-white' style='cursor:pointer; width:100px;'
+                             onclick=Accept('/api/volunteerdelivery/Accept?id='+${row.deliveryID})>
+                            Accept 
+                        </a></td><br><td>
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;'
+                             onclick=Reject('/api/volunteerdelivery/Reject?id='+${row.deliveryID})>
+                            Reject
+                        </a></td><br><td>
+                        <a class='btn btn-warning text-white' style='visibility:hidden;cursor:pointer; width:100px;'
+                             onclick=Complete('/api/volunteerdelivery/Complete?id='+${row.deliveryID})>
+                            Complete
+                        </a>
+                        </td>`
+                    }
+                    else if (data == 1) {
+                        return `<td></td>
+                                <td></td>
+                                <td>
+                                <a class='btn btn-warning text-white' style='cursor:pointer; width:120px;'
+                                onclick=Complete('/api/volunteerdelivery/Complete?id='+${row.deliveryID})>
+                                Complete &nbsp;<i class="bi bi-check-square-fill"></i>
+                                </a>
+                                </td>`
+                    }
+                    else if (data == 2) {
+                        return `<td></td>
+                                <td></td>
+                                <td></td>`
+                    }
+                }, "width": "10%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Accept(url) {
+    $.ajax({
+        type: "POST",
+        url: url,
+        success: function (data) {
+            if (data.success) {
+                dataTable.ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+                dataTable.ajax.reload();
+            }
+        }
+    });
+}
+function Reject(url) {
+    $.ajax({
+        type: "POST",
+        url: url,
+        success: function (data) {
+            if (data.success) {
+                dataTable.ajax.reload();
+            }
+            else {
+                dataTable.ajax.reload();
+            }
+        }
+    });
+}
+function Complete(url) {
+    $.ajax({
+        type: "POST",
+        url: url,
+        success: function (data) {
+            if (data.success) {
+                dataTable.ajax.reload();
+            }
+            else {
+                dataTable.ajax.reload();
+            }
+        }
+    });
+
+}
+        
---
+++ wwwroot/js/validationricky.js
@@ -0,0 +1,127 @@
+﻿var errorMessage = "";
+
+function ValidateDryFood() {
+    var message = document.getElementById('errorMessagediv');
+    errorMessage = "";
+    message.innerHTML = "";
+    var has_error = false;
+    if (!validateName()) {
+        
+        has_error = true;
+    }
+    if (!validateDate()) {
+        
+        has_error = true;
+        
+    }
+    
+
+    if (has_error) {
+        message.innerHTML = errorMessage;
+        message.style.display = "unset";
+        
+        return false;
+    }
+    else {
+        return true;
+    }
+
+    
+    
+}
+function validateDate() {
+
+    if (document.getElementById("pickDate").value == "") {
+        return false;
+    }
+    
+    var todaysDate = new Date();
+    var deliveryMethod = document.getElementById('deliveryMethod');
+
+    var month;
+    if ((todaysDate.getMonth() + 1) < 10) {
+        month = "0" + (todaysDate.getMonth() + 1);
+    }
+    else {
+        month = (todaysDate.getMonth() + 1);
+    }
+    var birthDate = new Date(document.getElementById("pickDate").value);
+    var datetime = todaysDate.getFullYear() + "-" + month + "-" + todaysDate.getDate() + "T" + todaysDate.getHours() + ":" + todaysDate.getMinutes();
+    var currYear = todaysDate.getFullYear();
+    var birthYear, birthString;
+    if (document.getElementById("pickDate").value != "") {
+        if ((birthDate.getMonth() + 1) < 10) {
+            month = "0" + (birthDate.getMonth() + 1);
+        } else {
+            month = (birthDate.getMonth() + 1);
+        }
+        birthString = birthDate.getFullYear() + "-" + month + "-" + birthDate.getDate() + "T" + birthDate.getHours() + ":" + birthDate.getMinutes();
+
+        birthYear = birthDate.getFullYear();
+        if (birthYear < currYear) {
+
+            errorMessage += '<ul><li>Pick-up date should be after the current date</li></ul>';
+            return false;
+        }
+        else {
+            if (birthDate.getMonth() + 1 < todaysDate.getMonth() + 1) {
+                errorMessage += '<ul><li>Pick-up date should be after the current date</li></ul>';
+                return false
+            }
+            else {
+                if (birthDate.getMonth() == todaysDate.getMonth()) {
+                    if (birthDate.getDate() <= todaysDate.getDate()) {
+                        if (deliveryMethod.value == "448 Delivery") {
+                            if (birthDate.getDate() == todaysDate.getDate()) {
+
+
+                                errorMessage += '<ul><li>Since 448 Delivery is choosed, pick-up date should be one day after the current date.</li></ul>';
+
+                                return false;
+                            }
+                            else {
+                                errorMessage += '<ul><li>Pick-up date should be after the current date</li></ul>';
+
+                                return false
+                            }
+                        }
+                        else {
+                            if (birthDate.getDate() == todaysDate.getDate()) {
+                                return true;
+                            }
+                            else {
+                                errorMessage += '<ul><li>Pick-up date should be after the current date</li></ul>';
+
+                                return false;
+                            }
+                        }
+
+
+                    }
+                }
+                
+            }
+        }
+        
+        
+            return true;
+        
+
+    }
+}
+function validateName() {
+    var name = document.getElementById('dryName').value;
+    if (name != "") {
+        if (name.match(/^[0-9]+$/) != null) {
+            errorMessage += '<ul><li>Name cannot be number only</li></ul>';
+            
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return true;
+    }
+}
