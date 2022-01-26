c44d0e515320ac3686d7cb16bcd5732c1efba0d9
Lim Sin Jie <84025799+SinJie00@users.noreply.github.com>
2022-01-27 02:38:18 +0800

    Update Index.cshtml.cs

--- Pages/Deliveries/Index.cshtml.cs
+++ Pages/Deliveries/Index.cshtml.cs
@@ -18,25 +18,7 @@ namespace ZeroHunger.Pages.Deliveries
         }
         public void OnGet()
         {
-            Deliveries = _db.Delivery.Include(d => d.Volunteer).Include(d=>d.Receiver);
-            ArrayList message = new ArrayList();
-            foreach (var delivery in Deliveries)
-            {
-                if (delivery.DeliveryStatus.Equals(3))
-                {
-                    var id = delivery.DeliveryID.ToString();
-                    message.Add("Delivery with ID " + id + " is rejected. Please find a new volunteer.");
-                }
-            }
-            foreach (var delivery in Deliveries)
-            {
-                if (_db.DeliveryItem.Find(delivery.DeliveryID) == null)
-                {
-                    var delid = delivery.DeliveryID.ToString();
-                    message.Add("Delivery with ID " + delid + " has no delivery item. Please add delivery item now.");
-                }
-            }
-            ViewData["Message"] = message;
+           
         }
     }
 }
